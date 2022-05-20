f66d34a6cf7538a8a55cfe23663cd77c237b5433
Chris <skylake112@outlook.com>
2022-05-20 11:25:59 -0600

    skaffold the folders a bit more

---
+++ pages/components/head/head.tsx
@@ -0,0 +1,5 @@
+import { Head } from "next";
+
+const Goofy = () => <Head>
+
+</Head>
---
+++ pages/projects/index.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from "next"
+import Head from ''
+
+const Projects: NextPage = () => {
+
+}
